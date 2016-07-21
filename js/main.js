08584f5fc2db785165eacb0231669928e848aca6
DJBillieVacances <bhg3110@yahoo.de>
2016-07-21 11:07:23 -0500

    New Design
    Works well, but needs to improve much more

--- js/main.js
+++ js/main.js
@@ -1,54 +1,74 @@
-var imgContainer, pfDescBox, pfLinkContainer, pfTagsPlaceholder, pfTags;
+var imgContainer, pfDescBox, pfLinkContainer, pfTagsPlaceholder;
 var pfTopRow, pfMidRow, pfBottomRow;
 var previousButtonContainer, nextButtonContainer;
 
+var viewportHeight = document.documentElement.clientHeight;
+
 function getElements() {
-	imgContainer = document.getElementById("pf-img-container");
-	pfDescBox = document.getElementById("pf-description-box");
-	pfDescText = document.getElementById("pf-description-text");
-	pfLinkContainer = document.getElementById("pf-link-container");
-	pfTagsPlaceholder = document.getElementById("pf-tags-placeholder");
-	pfTags = document.getElementById("");
-	pfTopRow = document.getElementById("pf-top-row");
-	pfMidRow = document.getElementById("pf-mid-row");
-	pfBottomRow = document.getElementById("pf-bottom-row");
-	previousButtonContainer = document.getElementById("btn-previous-container");
-	nextButtonContainer = document.getElementById("btn-next-container");
+	imgContainer = document.querySelectorAll(".pf-img-container");
+	pfDescBox = document.querySelectorAll(".pf-description-box");
+	pfLinkContainer = document.querySelectorAll(".pf-link-container");
+	pfTagsPlaceholder = document.querySelectorAll(".pf-tags-placeholder");
+	pfTopRow = document.querySelectorAll(".pf-top-row");
+	pfMidRow = document.querySelectorAll(".pf-mid-row");
+	pfBottomRow = document.querySelectorAll(".pf-bottom-row");
+	previousButtonContainer = document.querySelectorAll(".btn-previous-container");
+	nextButtonContainer = document.querySelectorAll(".btn-next-container");
 }
 
 function calculateImgContainerHeight() {
+
 	// Calculate the image container height, it is set to its width which is 100% of the mid column
-	var imgContainerWidth = imgContainer.offsetWidth;
+	var imgContainerWidth = imgContainer[0].offsetWidth;
 	var imgContainerHeight = (imgContainerWidth * 0.625)
-	var toEval1 = 'imgContainer.style.height = "' + imgContainerHeight + 'px"';
-	eval(toEval1);
+	for (var a = 0; a < imgContainer.length; a++) {
+		var toEvalA = 'imgContainer[' + a + '].style.height = "' + imgContainerHeight + 'px"';
+		eval(toEvalA);
+	}
 
 	// We need to adjust the height of the rows accordingly now
-	var toEval2 = 'pfMidRow.style.height = "' + imgContainerHeight + 'px"';
-	eval(toEval2);
-	var viewportHeight = document.documentElement.clientHeight;
-	var toEval6 = 'pfTopRow.style.height = "' + ((viewportHeight - imgContainerHeight) / 2) + 'px"';
-	eval(toEval6);
-	var toEval7 = 'pfBottomRow.style.height = "' + ((viewportHeight - imgContainerHeight) / 2) + 'px"';
-	eval(toEval7);
+	for (var b = 0; b < pfTopRow.length; b++) {
+		var toEvalB = 'pfTopRow[' + b + '].style.height = "' + ((viewportHeight - imgContainerHeight) / 2) + 'px"';
+		eval(toEvalB);
+	}
+	for (var c = 0; c < pfMidRow.length; c++) {
+		var toEvalC = 'pfMidRow[' + c + '].style.height = "' + imgContainerHeight + 'px"';
+		eval(toEvalC);
+	}
+	for (var d = 0; d < pfBottomRow.length; d++) {
+		var toEvalD = 'pfBottomRow[' + d + '].style.height = "' + ((viewportHeight - imgContainerHeight) / 2) + 'px"';
+		eval(toEvalD);
+	}
 
 	// Now the width of the description box & the link container to its left
-	var toEval3 = 'pfDescBox.style.width = "' + (imgContainerWidth * (0.6)) + 'px"';
-	eval(toEval3);
-	var toEval5 = 'pfDescBox.style.left = "' + (imgContainerWidth / 2) + 'px"';
-	eval(toEval5);
-	var toEval4 = 'pfLinkContainer.style.width = "' + (imgContainerWidth / 2) + 'px"';
-	eval(toEval4);
+	for (var e = 0; e < pfDescBox.length; e++) {
+		var toEvalE = 'pfDescBox[' + e + '].style.width = "' + (imgContainerWidth * (0.6)) + 'px"';
+		eval(toEvalE);
+	}
+	for (var g = 0; g < pfDescBox.length; g++) {
+		var toEvalG = 'pfDescBox[' + g + '].style.left = "' + (imgContainerWidth / 2) + 'px"';
+		eval(toEvalG);
+	}
+	for (var h = 0; h < pfLinkContainer.length; h++) {
+		var toEvalH = 'pfLinkContainer[' + h + '].style.width = "' + (imgContainerWidth / 2) + 'px"';
+		eval(toEvalH);
+	}
 
 	// Same for the tags to be aligned at the bottom
-	var toEval8 = 'pfTagsPlaceholder.style.height = "' + (((viewportHeight - imgContainerHeight) / 2) - 37) + 'px"';
-	eval(toEval8);
+	for (var j = 0; j < pfTagsPlaceholder.length; j++) {
+		var toEvalJ = 'pfTagsPlaceholder[' + j + '].style.height = "' + (((viewportHeight - imgContainerHeight) / 2) - 37) + 'px"';
+		eval(toEvalJ);
+	}
 
 	// Position the previous/next buttons
-	var toEval9 = 'previousButtonContainer.style.top = "' + ((viewportHeight - 24) / 2) + 'px"';
-	eval(toEval9);
-	var toEval10 = 'nextButtonContainer.style.top = "' + ((viewportHeight - 24) / 2) + 'px"'; 
-	eval(toEval10);
+	for (var k = 0; k < previousButtonContainer.length; k++) {
+		var toEvalK = 'previousButtonContainer[' + k + '].style.top = "' + ((viewportHeight - 24) / 2) + 'px"';
+		eval(toEvalK);
+	}
+	for (var l = 0; l < nextButtonContainer.length; l++) {
+		var toEvalL = 'nextButtonContainer[' + l + '].style.top = "' + ((viewportHeight - 24) / 2) + 'px"';
+		eval(toEvalL);
+	}
 }
 
 window.onload = function() {
